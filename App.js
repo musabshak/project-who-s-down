3c1048db11e83ecb99c04bc0e69d77862caf39bd
Aarish Iyer <aarish407@gmail.com>
2020-05-19 15:55:17 -0400

    First commit for scaffolding

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Who's Down app!</Text>
+      <Text>This is Aarish</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
