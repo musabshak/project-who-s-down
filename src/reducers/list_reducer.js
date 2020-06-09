42f855781dcebc8446c27b26533b4e4421c7d93b
arjun-bhatt <46908672+arjun-bhatt@users.noreply.github.com>
2020-06-08 23:58:07 -0400

    Revert "Event list settings merged"

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -2,10 +2,6 @@ import { ActionTypes } from '../components/event_list/actions';
 
 const initialState = {
   all: [],
-  myEvents:[],
-  subscribedEvents: [],
-  imdownEvents: [],
-  error: null,
 };
 
 
@@ -13,26 +9,11 @@ const alleventsReducer = (state = initialState, action) => {
   switch (action.type) {
   case ActionTypes.FETCH_EVENTS:
     console.log('inside reducer fetch all events case');
-    return {all: action.payload.events};
-  case ActionTypes.FETCH_SUBSCRIBED_EVENTS:
-    return { error: action.error, subscribedEvents: action.payload, all: state.all };
-  case ActionTypes.SUBSCRIBE_EVENT:
-    return { error: action.error, subscribedEvents: state.subscribedEvents, all: state.all };
-  case ActionTypes.UNSUBSCRIBE_EVENT:
-    return { error: action.error, subscribedEvents: state.subscribedEvents, all: state.all };
-  case ActionTypes.FETCH_IMDOWN_EVENTS:
-    return { ...state, error: action.error, imdownEvents: action.payload};
-  case ActionTypes.FETCH_MY_EVENTS:
-      return { ...state, error: action.error, myEvents: action.payload};
-  case ActionTypes.IMDOWN_EVENT:
-    return { ...state, error: action.error};
-  case ActionTypes.UNIMDOWN_EVENT:
-    return { ...state, error: action.error};
+    return {all: action.payload.events}; 
   default:
-    console.log('inside reducer cant fetch cases');
+    console.log('inside reducer cant fetch case');
     return state;
   }
 };
 
 export default alleventsReducer;
-
